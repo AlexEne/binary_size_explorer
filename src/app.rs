c356d05c7c708a1ece13bf9a13fff804974fd349
Alexandru Ene <alexene@amazon.com>
2025-05-14 17:03:23 +0100

    remove unused stuff

--- src/app.rs
+++ src/app.rs
@@ -401,17 +401,3 @@ impl TemplateApp {
         self.analyzer_state = next_state;
     }
 }
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
-}
